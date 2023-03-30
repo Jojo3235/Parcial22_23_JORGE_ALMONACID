3abf420343afd62c981cb9069517ba28bfeb0830
Jojo3235 <joralmand@gmail.com>
2023-03-30 09:01:45 +0200

    ej 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -12,3 +12,16 @@ class Alumno:
 
     def __str__(self):
         return self.nombre + " " + str(self.nota)
+    
+def main():  
+    alumno1 = Alumno("Juan", 6)
+    alumno2 = Alumno("Pedro", 4)
+    alumno3 = Alumno("Maria", 7)
+    alumno4 = Alumno("Ana", 3)
+    print(alumno1)
+    print(alumno2)
+    print(alumno3)
+    print(alumno4)
+
+if __name__ == "__main__":
+    main()
