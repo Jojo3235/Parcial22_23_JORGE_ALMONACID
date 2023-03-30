688ec50c2374b83bbb0fa6c03dabd0a59c75288c
Jojo3235 <joralmand@gmail.com>
2023-03-30 09:58:48 +0200

    ej4 done

--- ej4/tests/tests_ejercicio4.py
+++ ej4/tests/tests_ejercicio4.py
@@ -10,3 +10,10 @@ class TestAlumno(unittest.TestCase):
             alumno2 = Alumno("Pedro", 3)
             self.assertEqual(alumno1.calificacion(), "Aprobado")
             self.assertEqual(alumno2.calificacion(), "Suspendido")
+
+        def test_str(self):
+            alumno1 = Alumno("Juan", 10)
+            alumno2 = Alumno("Pedro", 3)
+            self.assertEqual(str(alumno1), "Juan 10")
+            self.assertEqual(str(alumno2), "Pedro 3")
+            
